57c70aa14da302b8c7bad28b41be1a4e738b7f53
Kunj <shahkunj42@gmail.com>
2022-07-20 10:58:06 -0400

    commit tree 2ff5dbcbff9d409d7a757cb989ce2884c359937c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def not_zero?
+    self.side1 > 0 && self.side2 > 0 && self.side3 > 0
+  end
+
+  def side_length?
+    (self.side1 + self.side2) > self.side3 && (self.side1 + self.side3) > self.side2 && (self.side2 + self.side3) > self.side1
+  end
+
+  def kind
+    if(self.not_zero? && self.side_length?)
+      if (@side1 == @side2 && @side2 == @side3)
+        :equilateral
+      elsif(@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end 
+
+  class TriangleError < StandardError
+
+  end
 end
